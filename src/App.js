76d56f229a3aae434a04da9222e06fb637388f08
Jonathan Alvares <jonathan.alvares@p360.com>
2022-01-29 12:03:27 +0530

    Created todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
+let counter = 0;
 function App() {
+  const [task, setTask] = useState("");
+  const [list, setList] = useState([]);
+
+  function deleteItem(itemID) {
+    setList(list.filter((item) => item.id !== itemID));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setList((old) => {
+            setTask("");
+            return [...old, { todo: task, id: counter++ }];
+          });
+        }}
+      >
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => {
+            setTask(e.target.value);
+          }}
+        />
+      </form>
+      <button
+        onClick={() => {
+          setList([...list, task]);
+          setTask("");
+        }}
+      >
+        Add
+      </button>
+      <ul>
+        {list.map((item) => (
+          <div>
+            <li key={item.id}>{item.todo}</li>
+            <button
+              onClick={() => {
+                deleteItem(item.id);
+              }}
+            >
+              delete
+            </button>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
